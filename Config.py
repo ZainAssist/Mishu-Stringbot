c25abdda5d9735261f06f9e13d30f0863a1d5687
Ȥ⋏ιꪀ <158439852+ZainAssist@users.noreply.github.com>
2025-07-03 11:43:11 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -2,3 +2,7 @@
 API_ID = "28xxxx10"  # Replace with your API ID Get From my.telegram.org
 API_HASH = "7fc5b356924ghfgggv481ab5eca3"  # Replace with your API Hash Get From my.telegram.org
 BOT_TOKEN = "7824jjhhmd3gjq8qddzhyL0"  # Replace with your Bot Token Get From @BotFather 
+DB_URL= #give mongo
+MUST_JOIN= #channel username
+ADMINS = #Admin id
+OWNER_ID = #id of owner
