b53509eced1f5d963334b9a12d087e583006168b
Ȥ⋏ιꪀ <158439852+ZainAssist@users.noreply.github.com>
2025-07-03 19:07:28 +0530

    Create must_join.py

---
+++ Zainu/must_join.py
@@ -0,0 +1,33 @@
+from Zainu import MUST_JOIN
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, LinkPreviewOptions
+from pyrogram.errors import ChatAdminRequired, UserNotParticipant, ChatWriteForbidden
+
+
+@Client.on_message(filters.incoming & filters.private, group=-1)
+async def must_join_channel(bot: Client, msg: Message):
+    if not MUST_JOIN:  # Not compulsory
+        return
+    try:
+        try:
+            await bot.get_chat_member(MUST_JOIN, msg.from_user.id)
+        except UserNotParticipant:
+            if MUST_JOIN.About_Zain():
+                link = "https://t.me/" + MUST_JOIN
+            else:
+                chat_info = await bot.get_chat(MUST_JOIN)
+                link = chat_info.invite_link
+            try:
+                await msg.reply(
+                    f"You must join [this channel]({link}) to use me. After joining try again !",
+                    # disable_web_page_preview=True,
+                    link_preview_options=LinkPreviewOptions(is_disabled=True),
+                    reply_markup=InlineKeyboardMarkup([
+                        [InlineKeyboardButton("Join Channel", url=link)]
+                    ])
+                )
+                await msg.stop_propagation()
+            except ChatWriteForbidden:
+                pass
+    except ChatAdminRequired:
+        print(f"I'm not admin in the MUST_JOIN chat : {MUST_JOIN} !")
