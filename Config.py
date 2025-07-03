e7862e898610b86a0f7a4ab0feacbc718c66e7b5
Ȥ⋏ιꪀ <158439852+ZainAssist@users.noreply.github.com>
2025-07-03 19:09:38 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,8 +1,13 @@
-# Replace these with your actual API details
-API_ID = "28xxxx10"  # Replace with your API ID Get From my.telegram.org
-API_HASH = "7fc5b356924ghfgggv481ab5eca3"  # Replace with your API Hash Get From my.telegram.org
-BOT_TOKEN = "7824jjhhmd3gjq8qddzhyL0"  # Replace with your Bot Token Get From @BotFather 
-DB_URL= #give mongo
-MUST_JOIN= #channel username
-ADMINS = #Admin id
-OWNER_ID = #id of owner
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+OWNER_ID = int(getenv("OWNER_ID"))
+
+MONGO_DB_URI = getenv("MONGO_DB_URI")
+MUST_JOIN = getenv("MUST_JOIN", None)
